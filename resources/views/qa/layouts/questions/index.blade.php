97a7292aff8a2ef9f2540f61f1fb989c8f694345
prathzzzz <kariapratham@gmail.comz>
2024-06-03 12:54:55 +0530

    chore : create questions page and show users

---
+++ resources/views/qa/layouts/questions/index.blade.php
@@ -0,0 +1,24 @@
+@extends('qa.layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>All Questions</h2>
+                    </div>
+
+                    @foreach ($questions as $question)
+                        <div class="card-body">
+                            <div class="card-title">
+                                <h3>{{ $question->title }}</h3>
+                                <p>{{ Str::limit($question->body, 250) }}</p>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
