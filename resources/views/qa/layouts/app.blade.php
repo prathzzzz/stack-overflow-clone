8db1795ddbdfb6b877ee1c8a69bc5e11d0e9e5db
prathzzzz <kariapratham@gmail.comz>
2024-06-03 12:50:51 +0530

    chore : create skeleton

---
+++ resources/views/qa/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'All your questions will be answered here')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    @yield('page-level-styles')
+</head>
+
+<body>
+    @include('qa.layouts.partials._navbar')
+    @yield('content')
+
+    @yield('page-level-scripts')
+    @include('qa.layouts.partials._footer')
+</body>
+
+</html>
