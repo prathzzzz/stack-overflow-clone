51c4780a94b14bf624a3e76930e764ec10b6706f
prathzzzz <kariapratham@gmail.comz>
2024-06-03 14:18:15 +0530

    chore : display question list complete

--- resources/views/qa/layouts/questions/index.blade.php
+++ resources/views/qa/layouts/questions/index.blade.php
@@ -8,17 +8,43 @@
                     <div class="card-header">
                         <h2>All Questions</h2>
                     </div>
-
-                    @foreach ($questions as $question)
-                        <div class="card-body">
-                            <div class="card-title">
-                                <h3>{{ $question->title }}</h3>
-                                <p>{{ Str::limit($question->body, 250) }}</p>
+                    <div class="row">
+                        @foreach ($questions as $question)
+                            <div class="col-md-2">
+                                <div class="d-flex flex-column mr-4 statistics">
+                                    <div class="votes text-center mb-3">
+                                        <strong class="d-block"> {{ $question->votes_count }} </strong> Votes
+                                    </div>
+                                    <div class="text-center mb-3">
+                                        <div class="answers {{ $question->answer_style }}" style="max-width: 60%; margin:0 auto;">
+                                            <strong class="d-block"> {{ $question->answers_count }}</strong>Answers
+                                        </div>
+                                    </div>
+                                    <div class="views text-center">
+                                        <strong class="d-block">{{ $question->views_count }}</strong> Views
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
+
+                            <div class="col-md-10">
+                                <div class="card-body">
+                                    <div class="card-title">
+                                        <h3>
+                                            <a href="{{ $question->url }}">{{ $question->title }}</a>
+                                        </h3>
+                                        <p>Asked By: <a href="#">{{ $question->owner->name }}</a>
+                                            <span class="text-muted">{{ $question->created_date }}</span>
+                                        </p>
+                                        <p>{{Str::limit($question->body),250}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <hr>
+                        @endforeach
+                    </div>
+
                     <div class="card-footer">
-                        {{$questions->links()}}
+                        {{ $questions->links() }}
                     </div>
                 </div>
             </div>
