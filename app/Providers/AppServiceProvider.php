6dea9da0d492252972503fdb46a4ba4a4618b39a
prathzzzz <kariapratham@gmail.comz>
2024-06-04 12:30:53 +0530

    chore : "Binding custom route"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Question;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,5 +24,9 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Paginator::useBootstrapFive();
+        Route::bind('slug',function(string $slug)
+        {
+            return Question::where('slug',$slug)->firstOrFail();
+        });
     }
 }
