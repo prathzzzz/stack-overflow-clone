dbb6a0ba63ed8eede753c5d655817ebb42cb5242
prathzzzz <kariapratham@gmail.comz>
2024-06-04 11:27:27 +0530

    chore : add edit button and change the route

--- resources/views/qa/layouts/questions/index.blade.php
+++ resources/views/qa/layouts/questions/index.blade.php
@@ -7,7 +7,7 @@
                 <div class="card">
                     <div class="card-header d-flex justify-content-between">
                         <h2>All Questions</h2>
-                        <a href="{{route('questions.create')}}" class="btn btn-outline-primary">Ask a Question!</a>
+                        <a href="{{ route('questions.create') }}" class="btn btn-outline-primary">Ask a Question!</a>
                     </div>
                     <div class="row">
                         @foreach ($questions as $question)
@@ -17,7 +17,8 @@
                                         <strong class="d-block"> {{ $question->votes_count }} </strong> Votes
                                     </div>
                                     <div class="text-center mb-3">
-                                        <div class="answers {{ $question->answer_style }}" style="max-width: 60%; margin:0 auto;">
+                                        <div class="answers {{ $question->answer_style }}"
+                                            style="max-width: 60%; margin:0 auto;">
                                             <strong class="d-block"> {{ $question->answers_count }}</strong>Answers
                                         </div>
                                     </div>
@@ -30,13 +31,20 @@
                             <div class="col-md-10">
                                 <div class="card-body">
                                     <div class="card-title">
-                                        <h3>
-                                            <a href="{{ $question->url }}">{{ $question->title }}</a>
-                                        </h3>
+                                        <div class="d-flex justify-content-between">
+                                            <h3>
+                                                <a href="{{ $question->url }}">{{ $question->title }}</a>
+                                            </h3>
+                                            <div class="div">
+                                                <a href="{{ route('questions.edit', $question->id) }}"
+                                                    class="btn btn-sm btn-outline-warning">Edit</a>
+                                            </div>
+                                        </div>
+
                                         <p>Asked By: <a href="#">{{ $question->owner->name }}</a>
                                             <span class="text-muted">{{ $question->created_date }}</span>
                                         </p>
-                                        <p>{!!Str::limit($question->body,250)!!}</p>
+                                        <p>{!! Str::limit($question->body, 250) !!}</p>
                                     </div>
                                 </div>
                             </div>
