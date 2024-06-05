85485d655f090486d6aa35dc725eec0b8479309d
prathzzzz <kariapratham@gmail.com>
2024-06-05 12:10:16 +0530

    refactor : UI changes

--- resources/views/qa/layouts/questions/show.blade.php
+++ resources/views/qa/layouts/questions/show.blade.php
@@ -1,30 +1,50 @@
 @extends('qa.layouts.app')
-
+@section('page-level-styles')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+@endsection
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h1>{{$question->title}}</h1>
+                        <h1>{{ $question->title }}</h1>
                     </div>
                     <div class="card-body">
-                        {!! $question->body!!}
+                        {!! $question->body !!}
                     </div>
                     <div class="card-footer">
                         <div class="d-flex justify-content-between mr-3">
-                            <div></div>
+                            <div>
+                                <div class="d-flex">
+                                    <div>
+                                        <a href="#" title="Up Vote" class="vote-up d-block text-center text-dark">
+                                            <i class="fa fa-caret-up fa-3x"></i>
+                                        </a>
+                                        <h4 class="votes-count textmuted text-center m-0">{{ $question->votes_count }} </h4>
+                                        <a href="#" title="Down Vote" class="vote-up d-block text-center text-dark">
+                                            <i class="fa fa-caret-down fa-3x"></i>
+                                        </a>
+                                    </div>
+                                    <div class="ml-4 mt-2">
+                                        <a href="#" title="Mark as Fav">
+                                            <i class="fa fa-star fa-2x text-dark"></i>
+                                        </a>
+                                        <h4>123</h4>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="d-flex flex-column">
                                 <div class="text-muted mb-2 text-right">
-                                    Asked At: {{$question->created_date}}
+                                    Asked At: {{ $question->created_date }}
                                 </div>
                                 <div class="d-flex mb-2 ">
                                     <div>
-                                        <img src="{{ $question->owner->avatar}}" alt="Avatar of{{
-                                        $question->owner->name }}">
+                                        <img src="{{ $question->owner->avatar }}"
+                                            alt="Avatar of{{ $question->owner->name }}">
                                     </div>
                                     <div class="m-2">
-                                        {{$question->owner->name}}
+                                        {{ $question->owner->name }}
                                     </div>
                                 </div>
                             </div>
@@ -38,25 +58,25 @@
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3>{{Str::plural('Answer',$question->answers_count)}}</h3>
+                        <h3>{{ Str::plural('Answer', $question->answers_count) }}</h3>
                     </div>
                     <div class="card-body">
-                        @foreach ($question->answers as $answer )
-                            {!! $answer->body!!}
+                        @foreach ($question->answers as $answer)
+                            {!! $answer->body !!}
                             <div class="d-flex justify-content-between mr-3">
                                 <div></div>
                                 <div class="d-flex flex-column">
                                     <div class="text-muted mb-2 text-right">
-                                        Answered At: {{$answer->created_date}}
+                                        Answered At: {{ $answer->created_date }}
                                     </div>
                                     <div class="d-flex mb-2">
                                         <div>
-                                            <img src="{{$answer->author->avatar}}" alt="Avatar of{{
-                                            $answer->author->name}}">
+                                            <img src="{{ $answer->author->avatar }}"
+                                                alt="Avatar of{{ $answer->author->name }}">
                                         </div>
                                         <div>
                                             <div class="m-2">
-                                                {{$answer->author->name}}
+                                                {{ $answer->author->name }}
                                             </div>
                                         </div>
                                     </div>
