0005b4b363b97059317e348d53431230b939bf7a
prathzzzz <kariapratham@gmail.com>
2024-06-05 10:39:33 +0530

    fix : remove answer count

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -21,7 +21,7 @@ class QuestionFactory extends Factory
             'title' => rtrim(fake()->sentence(rand(5,10)),'.'),
             'body' => fake()->paragraphs(rand(3,7),true),
             'views_count' => rand(0,100),
-            'answers_count' => rand(0,10),
+            // 'answers_count' => rand(0,10),
             'votes_count' => rand(-10,10)
         ];
     }
