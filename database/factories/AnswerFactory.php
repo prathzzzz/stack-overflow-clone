a8d065986cd475dd6cddab800b01c14ff55d23e0
prathzzzz <kariapratham@gmail.com>
2024-06-05 10:33:51 +0530

    chore : create answer factory

---
+++ database/factories/AnswerFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Answer>
+ */
+class AnswerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'body' => fake()->paragraph(rand(3,5),true),
+            'user_id' => User::pluck('id')->random(),
+            'votes_count' => rand(-10,10)
+        ];
+    }
+}
