7b78744bb201a522d3769ae6e5d1b0c7677c7e07
prathzzzz <kariapratham@gmail.com>
2024-06-05 14:15:48 +0530

    chore : create answer controller

---
+++ app/Http/Controllers/AnswersController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Answers\UpdateAnswerRequest;
+use App\Http\Requests\Questions\CreateAnswerRequest;
+use App\Models\Answer;
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class AnswersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CreateAnswerRequest $request, Question $question)
+    {
+        //
+        $question->answers()->create(
+            [
+                'body' => $request->body,
+                'user_id' => auth()->id()
+            ]
+        );
+
+        session()->flash('success', 'Your answer was submitted successfully');
+        return redirect($question->url);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Answer $answer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Question $question,Answer $answer)
+    {
+        //
+        return view('qa.answers.edit',compact('question','answer'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAnswerRequest $request, Question $question, Answer $answer)
+    {
+        //
+        $answer->update([
+            'body' => $request->body,
+        ]);
+
+        session()->flash('success', 'Your answer was updated successfully');
+        return redirect($question->url);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Question $question, Answer $answer)
+    {
+        //
+        $answer->delete();
+        session()->flash('success', 'Your answer was deleted successfully');
+        return redirect($question->url);
+    }
+}
