70efb7e4b942ed963c6e675db632536c695e1225
prathzzzz <kariapratham@gmail.com>
2024-06-05 10:33:35 +0530

    chore : define answers relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
