28cd32746dfe43d7d1c9a2c3a95c348b259f95ad
prathzzzz <kariapratham@gmail.com>
2024-06-06 13:10:10 +0530

    chore : create helper for favortie count and define relation

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -52,4 +52,17 @@ class Question extends Model
         $this->best_answer_id = $answer->id;
         $this->save();
     }
+    public function favorites()
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+    public function getFavoritesCountAttribute()
+    {
+        return $this->favorites()->count();
+    }
+
+    public function getIsFavoritesAttribute()
+    {
+        return $this->favorites()->where('user_id', auth()->id())->count() > 0;
+    }
 }
