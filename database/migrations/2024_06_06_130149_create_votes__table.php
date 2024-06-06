513c856d45b47b1635e7d6f673370dbe6807d87f
prathzzzz <kariapratham@gmail.com>
2024-06-06 13:10:51 +0530

    chore : create votes table

---
+++ database/migrations/2024_06_06_130149_create_votes__table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('votes_', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('voter_id');
+            $table->string('vote_type');
+            $table->tinyInteger('vote');
+
+            $table->timestamps();
+
+            $table->unique(['user_id','vote_id','vote_type']);
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('votes_');
+    }
+};
