1db73c62cef90c4750221adaf5fee014969ada68
prathzzzz <kariapratham@gmail.com>
2024-06-06 15:06:49 +0530

    chore : create user controller

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    //
+    public function notifications()
+    {
+        auth()->user()->unreadNotifications->markAsRead();
+        $notifications = auth()->user()->notifications()->paginate(10);
+        return view('qa.users.notifications',compact(['notifications']));
+    }
+}
